54b2b8a467953928973f55b47e47046c68dd7231
zakester <Hadjammar.Hamza.Zakaria@gmail.com>
2023-04-14 21:21:52 +0100

    setup controller

--- src/main/java/com/s/challenge/MainController.java
+++ src/main/java/com/s/challenge/MainController.java
@@ -1,16 +1,21 @@
 package com.s.challenge;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
 
-    public void startTimer() {
+    @FXML public Label total_group_1, total_group_2;
+
+    @FXML public TextField arabic_group_1, islam_group_1, math_group_1, physics_group_1, scince_group_1, fr_group_1, generalColture_group_1, history_group_1, civil_group_1, info_group_1, english_group_1;
+    @FXML public TextField arabic_group_2, islam_group_2, math_group_2, physics_group_2, scince_group_2, fr_group_2, generalColture_group_2, history_group_2, civil_group_2, info_group_2, english_group_2;
+
+    @FXML public void startTimer() {
 
     }
 
-    public void showWinner() {
+    @FXML public void showWinner() {
 
     }
 }
